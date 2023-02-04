6eea46d3f542c086d7b92e7311607b672db79e9b
MilkeeyCat <milkeeycat@gmail.com>
2023-02-04 16:38:51 +0200

    Can you just start working :cry:

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -9,12 +9,12 @@ import { EventsRouter } from "./routes/events.route.js"
 import { GameServerService } from "./services/gameServer.service.js"
 import { ServersService } from "./services/servers.service.js"
 import { json } from "body-parser"
-import path from "path"
-
-const app = express()
-const port = 8080
 
 dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8080
+
 app.use("/public", express.static("./public"))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
